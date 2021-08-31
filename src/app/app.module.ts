25f49896f7a56c509985151e42d4ccc8bc635ef1
graceoswal <graceoswal88@gmail.com>
2021-08-31 16:38:41 +0300

    add form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import  { FormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +9,7 @@ import { StoreComponent } from './store/store.component';
 import { StoreDetailComponent } from './store-detail/store-detail.component';
 import { HighlightDirective } from './highlight.directive';
 import { DateCountPipe } from './date-count.pipe';
+import { StoreFormComponent } from './store-form/store-form.component';
 
 @NgModule({
   declarations: [
@@ -14,11 +17,13 @@ import { DateCountPipe } from './date-count.pipe';
     StoreComponent,
     StoreDetailComponent,
     HighlightDirective,
-    DateCountPipe
+    DateCountPipe,
+    StoreFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
