71a34793826ed84b82c1b5ced213303b46d91891
graceoswal <graceoswal88@gmail.com>
2021-08-31 03:13:37 +0300

    add properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,12 @@ export class AppComponent {
   title = 'Store';
 
   stores:Store[] = [
-    { customer_id:'#20010', product: 'juice', quantity:'1 litre',price: 'Ksh 122'}
+    { customer_id:'#20010', product: 'juice', quantity:'1 litre',price: 'Ksh 122'},
+        { customer_id:'#20013', product: 'jSalted Crisps', quantity:'50g',price: 'Ksh 30'},
+            { customer_id:'#20016', product: 'Brrokside', quantity:'500ml',price: 'Ksh 499'},
+                { customer_id:'#20011', product: 'Top fry', quantity:'5 litres',price: 'Ksh 1000'},
+                    { customer_id:'#20101', product: 'salt', quantity:'1kg',price: 'Ksh 56'},
+                        { customer_id:'#20034', product: 'Vinegar', quantity:'700ml',price: 'Ksh 79'},
+                        
   ];
 }
