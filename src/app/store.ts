d45762d6c75b99ab95885be5fa8439d34899f67b
graceoswal <graceoswal88@gmail.com>
2021-08-31 12:13:18 +0300

    add date function

--- src/app/store.ts
+++ src/app/store.ts
@@ -4,7 +4,9 @@ constructor(
   public customer_id: string,
   public product: string,
   public quantity: string,
-  public price: string){
+  public price: string,
+  public productDate: Date
+  ){
 
     this.showDescription=false;
   }
