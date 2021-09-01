a7df7920c838c9ecaca9cdb091b6812c9bd44270
graceoswal <graceoswal88@gmail.com>
2021-09-01 10:36:40 +0300

    add storelist file

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Store } from '../store';
 @Component({
   selector: 'app-store',
   templateUrl: './store.component.html',
@@ -7,15 +6,7 @@ import { Store } from '../store';
 })
 export class StoreComponent implements OnInit {
 
-  stores:Store[] = [
-    new Store( '#20010', 'juice','1 litre', 'Ksh 122', new Date(2021,4,5)),
-     new Store( '#20013' , 'Salted Crisps', '50g','Ksh 30', new Date (2022,4,4)),
-     new Store('#20016', 'Brookside', '500ml', 'Ksh 499',new Date(2021,5,8)),
-     new Store ('#20011',  'Top fry', '5 litres', 'Ksh 1000', new Date(2026,2,7)),
-      new Store('#20100', 'salt', '1kg', 'Ksh 56', new Date(2034,5,4)),
-    new Store ('#20034',  'Vinegar', '700ml', 'Ksh 79', new Date(2036,4,5)),
 
-  ];
 
   toggleDetails(index:any){
     this.stores[index].showDescription=!this.stores[index].showDescription;
---
+++ src/app/storeList.ts
@@ -0,0 +1,10 @@
+ import { Store} from './store';
+ export let stores:Store[] = [
+    new Store( '#20010', 'juice','1 litre', 'Ksh 122', new Date(2021,4,5)),
+     new Store( '#20013' , 'Salted Crisps', '50g','Ksh 30', new Date (2022,4,4)),
+     new Store('#20016', 'Brookside', '500ml', 'Ksh 499',new Date(2021,5,8)),
+     new Store ('#20011',  'Top fry', '5 litres', 'Ksh 1000', new Date(2026,2,7)),
+      new Store('#20100', 'salt', '1kg', 'Ksh 56', new Date(2034,5,4)),
+    new Store ('#20034',  'Vinegar', '700ml', 'Ksh 79', new Date(2036,4,5)),
+
+  ]
