302df811b31f3bb4e571ab20dac00c7c41bae1a1
graceoswal <graceoswal88@gmail.com>
2021-09-01 15:42:16 +0300

    add a 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StoreComponent } from './store/store.component';
 import { AboutusComponent } from './aboutus/aboutus.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
    { path: 'stores', component: StoreComponent},
   { path: 'aboutus', component: AboutusComponent},
+  {path: '**', component: NotFoundComponent},
+
+  {path: '', redirectTo:"/stores", pathMatch:"full"},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { DateCountPipe } from './date-count.pipe';
 import { StoreFormComponent } from './store-form/store-form.component';
 import { AboutusComponent } from './aboutus/aboutus.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { NavbarComponent } from './navbar/navbar.component';
     DateCountPipe,
     StoreFormComponent,
     AboutusComponent,
-    NavbarComponent
+    NavbarComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
